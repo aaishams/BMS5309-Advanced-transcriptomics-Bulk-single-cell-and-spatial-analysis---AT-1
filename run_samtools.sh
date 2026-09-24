00131dd7a24e90ee781e99a687be9c70f41bca36
aaishams <115831912+aaishams@users.noreply.github.com>
2026-09-24 23:09:15 +1000

    Create run_samtools.sh
    Mapping and Quantification of Transcripts - SAMtools

---
+++ run_samtools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash  
+input_dir=~/analysis/trimmomatic_hisat2 
+output_dir=~/analysis/aligned_sorted_bam  
+mkdir -p $output_dir  
+for file in $input_dir/*.sam  
+do   
+# Get the basename of the file minus the ".sam" ending   
+name=$(basename $file .sam)   
+echo $name   
+# Convert SAM to BAM format   
+samtools view -b $file > $output_dir/$name.unsorted.bam   
+# Sort BAM files   
+samtools sort $output_dir/$name.unsorted.bam >  
+$output_dir/$name.sorted.bam  
+# Index BAM files   
+samtools index $output_dir/$name.sorted.bam   
+# Generate summary stats   
+samtools flagstat $output_dir/$name.sorted.bam > 
+$output_dir/$name.flagstat   
+# Delete the unsorted BAM   
+rm $output_dir/$name.unsorted.bam  
+done 
