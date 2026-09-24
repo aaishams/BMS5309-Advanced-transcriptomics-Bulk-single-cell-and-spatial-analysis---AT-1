a663bbc96ab251d1301b2ff07cf0839171e847cb
aaishams <115831912+aaishams@users.noreply.github.com>
2026-09-24 23:14:20 +1000

    Create run_feature_counts.sh
    Mapping and Quantification of Transcripts - featureCounts (part of Subread)

---
+++ run_feature_counts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash  
+
+# Load and avail the required module
+module load subread
+module avail subread
+
+# Assign all the required directory locations
+reference_gtf="/home/amoh0254/mouse_reference/Mus_musculus.GRCm39.110.gtf"
+bam_dir="/home/amoh0254/amoh0254_BMS5309_AT1/hisat2"  
+output_dir="/home/amoh0254/amoh0254_BMS5309_AT1/feature_counts"
+output_file="combined_counts.txt"  
+mkdir -p "$output_dir"  
+
+# Run featureCounts
+featureCounts -p -t exon -g gene_id -s 0 \
+  -a "$reference_gtf" \
+  -o "$output_dir/$output_file" \
+  "$bam_dir"/*_R1.trim_pe.fastq_R1.trim_pe.fastq.hisat2.sorted.bam
